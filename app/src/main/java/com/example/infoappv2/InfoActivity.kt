828e718e7d11b9eb3e934227adf914f062727837
maxel <maximilian.elbers@hs-osnabrueck.de>
2020-07-27 21:38:12 +0200

    Initial commit!

--- app/src/main/java/com/example/infoappv2/InfoActivity.kt
+++ app/src/main/java/com/example/infoappv2/InfoActivity.kt
@@ -4,8 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class InfoActivity : AppCompatActivity() {
+
+    private val introSlideAdapter = IntroSlideAdapter(
+        listOf(
+            IntroSlide(
+                "Willkommen am Campus Lingen!",
+            "Die InfoApp der Hochschule Osnabrück bietet euch alle Informationen die Ihr zum Studienstart am Campus Lingen benötigt. \" +\n" +
+                    "          \"Außerdem bieten wir euch coole Spiele die Ihr spielen könnt während Ihr in der auf dem Campus wartet.",
+                R.drawable.hs_os
+            ),
+            IntroSlide(
+              "Alles was Du brauchst!",
+                "Hol dir Kontakte, Informationen, News und vieles mehr!",
+                R.drawable.firstimage
+            ),
+            IntroSlide(
+                "Schreibe mit Dozenten!",
+                "Um mit Dozenten schreiben zu könne muss eine Mobilfunknummer hinterlegt sein.",
+                R.drawable.ic_baseline_chat_24
+            )
+        )
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
     }
+
+    fun
 }
