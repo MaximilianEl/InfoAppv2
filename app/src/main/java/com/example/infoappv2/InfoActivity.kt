0d7aaf7f85654faa25a6ca73edde49b4de432a89
maxel <maximilian.elbers@hs-osnabrueck.de>
2020-07-27 22:35:33 +0200

    Initial commit!

--- app/src/main/java/com/example/infoappv2/InfoActivity.kt
+++ app/src/main/java/com/example/infoappv2/InfoActivity.kt
@@ -2,6 +2,7 @@ package com.example.infoappv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_info.*
 
 class InfoActivity : AppCompatActivity() {
 
@@ -29,7 +30,7 @@ class InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+        introSliderViewPager.adapter = introSlideAdapter
     }
 
-    fun
 }
