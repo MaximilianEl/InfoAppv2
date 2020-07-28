7b8f271f9820d66d1ebebdc794f30a2c7a698395
maxel <maximilian.elbers@hs-osnabrueck.de>
2020-07-28 16:26:17 +0200

    StartScreenActivity.kt, InfoActivity.kt and MenuActivity.kt

--- app/src/main/java/com/example/infoappv2/InfoActivity.kt
+++ app/src/main/java/com/example/infoappv2/InfoActivity.kt
@@ -1,7 +1,16 @@
 package com.example.infoappv2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_info.*
 
 class InfoActivity : AppCompatActivity() {
@@ -31,6 +40,73 @@ class InfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
         introSliderViewPager.adapter = introSlideAdapter
+        setupIndicators()
+        setCurrentIndicator(0)
+        introSliderViewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback(){
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicator(position)
+            }
+        })
+        buttonNext.setOnClickListener {
+            if (introSliderViewPager.currentItem + 1 < introSlideAdapter.itemCount) {
+                introSliderViewPager.currentItem += 1
+            }  else {
+                Intent(applicationContext, MenuActivity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
+        }
+        textSkipIntro.setOnClickListener {
+            Intent(applicationContext, MenuActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
     }
 
+    private fun setupIndicators() {
+        val indicators = arrayOfNulls<ImageView>(introSlideAdapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParams.setMargins(8,0,8,0)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(applicationContext)
+            indicators[i].apply {
+                this?.setImageDrawable(
+                ContextCompat.getDrawable(
+                    applicationContext,
+                    R.drawable.indicator_inactive
+                )
+            )
+                this?.layoutParams = layoutParams
+            }
+            indicatorsContainer.addView(indicators[i])
+        }
+    }
+
+    private fun setCurrentIndicator(index: Int) {
+        val childCount = indicatorsContainer.childCount
+        for (i in 0 until childCount) {
+            val imageView = indicatorsContainer[i] as ImageView
+            if (i == index) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active
+                    )
+                )
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive
+                    )
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/infoappv2/StartScreenActivity.kt
+++ app/src/main/java/com/example/infoappv2/StartScreenActivity.kt
@@ -1,11 +1,24 @@
 package com.example.infoappv2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.schedule
 
 class StartScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_startscreen)
+        openInfoActivity()
+    }
+
+    private fun openInfoActivity() {
+        Timer().schedule(3000){
+             Intent(applicationContext, InfoActivity::class.java).also {
+                 startActivity(it)
+             }
+             }
     }
 }
