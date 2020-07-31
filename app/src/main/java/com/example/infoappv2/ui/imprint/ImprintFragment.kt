29fe7fa17a8c13d7882874d0b4eb7de7c9445742
maxel <maximilian.elbers@hs-osnabrueck.de>
2020-07-31 19:39:00 +0200

    Imprint - The Mid-game

--- app/src/main/java/com/example/infoappv2/ui/imprint/ImprintFragment.kt
+++ app/src/main/java/com/example/infoappv2/ui/imprint/ImprintFragment.kt
@@ -1,5 +1,7 @@
 package com.example.infoappv2.ui.imprint
 
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.infoappv2.R
+import kotlinx.android.synthetic.main.fragment_imprint.*
 
 class ImprintFragment : Fragment() {
 
@@ -25,8 +28,35 @@ class ImprintFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        facebook.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/hs.osnabrueck"))
+            startActivity(i)
+        }
+
+        insta.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/hs_osnabrueck/"))
+            startActivity(i)
+        }
+
+        youtube.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/user/HochschuleOS"))
+            startActivity(i)
+        }
+
+        twitter.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/HS_Osnabrueck"))
+            startActivity(i)
+        }
+
+        website.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.hs-osnabrueck.de/"))
+            startActivity(i)
+        }
+
         viewModel = ViewModelProviders.of(this).get(ImprintViewModel::class.java)
         // TODO: Use the ViewModel
+
     }
 
 }
