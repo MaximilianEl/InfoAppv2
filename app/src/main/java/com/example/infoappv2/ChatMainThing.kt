56116ea141549f51a09c47af065fe18aa4562987
RealKrichter <RealKrichter@users.noreply.github.com>
2020-08-03 17:01:13 +0200

    Chatstuff geadded (Chat ist nicht mal ansatzweise da, da ich eure aufmerksamkeit habe erzähl ich euch ne Geschichte:
    Es war einmal ein kleines Mädchen, das von allen gemocht wurde. Ihre Großmutter schenkte ihr ein schönes rotes Käppchen aus Samt. Und weil ihr das Käppchen sehr gefiel, wollte sie es gar nicht mehr ausziehen. Darum wurde sie von allen Rotkäppchen genannt.
    
     Eines Tages sprach ihre Mutter zu ihr: “Rotkäppchen, ich gebe dir ein Körbchen mit einem Kuchen und einer Flasche Wein. Bringe dieses Körbchen bitte zur Großmutter hinaus. Sie ist krank und schwach und wird sich darüber freuen.” „Ja, das möchte ich gerne tun“ sagte Rotkäppchen zu ihrer Mutter.
    
     Die Großmutter wohnte draußen im Wald, eine halbe Stunde vom Dorf entfernt. Als Rotkäppchen den Wald betrat, begegnete ihr der der Wolf. Rotkäppchen wußte nicht, dass er böse war und fürchtete sich nicht vor ihm. “Guten Tag, Rotkäppchen!” sprach er. “Schönen Dank, Wolf.” “Wo willst du denn hin so früh, Rotkäppchen?” “Zur Großmutter.” “Was trägst du da in deinem Körbchen?” “Kuchen und Wein, damit sich die Großmutter daran stärken kann.” “Wo wohnt denn deine Großmutter?” “Noch eine gute Viertelstunde weiter im Wald, unter den drei großen Eichen steht ihr Haus.” sagte Rotkäppchen. Der Wolf dachte: “Das Kind wird mir noch besser schmecken als die Alte. Du musst dir einen Plan ausdenken, dass du beide erwischen kannst.”
    
     Daraufhin verabschiedete sich der Wolf und nahm eine Abkürzung. Am Haus der Großmutter klopfte er an die Türe. „Wer ist draußen?“ „Rotkäppchen, ich bringe dir Kuchen und Wein, mach auf!“ „Drück nur auf die Klinke,“ rief die Großmutter, „ich bin zu schwach und kann nicht aufstehen.“ Der Wolf drückte auf die Klinke, die Türe sprang auf und ohne ein Wort zu sprechen ging er direkt zum Bett der Großmutter und verschlang sie. Dann zog er ihre Kleider an, setzte ihre Haube auf, legte sich in ihr Bett und wartete.
    
     Rotkäppchen trat kurze Zeit später ein und rief „Guten Morgen, liebe Großmutter!“ bekam aber keine Antwort. Dann trat es ans Bett heran. Dort lag die Großmutter und hatte die Haube tief ins Gesicht gezogen und sah recht seltsam aus. „Ei, Großmutter, was hast du für große Ohren?“ „Dass ich dich besser hören kann.“ „Ei, Großmutter, was hast du für große Augen?“ „Dass ich dich besser sehen kann.“ „Ei, Großmutter, was hast du für große Hände?“ „Dass ich dich besser packen kann.“ „Aber, Großmutter, was hast du für ein entsetzlich großes Maul?“ „Daß ich dich besser fressen kann.“ Kaum hatte der Wolf das gesagt, sprang er aus dem Bette und verschlang das arme Rotkäppchen.
    
     Dann legte er sich wieder ins Bett, schlief ein und fing laut zu schnarchen an. Der Jäger ging am Haus vorbei und dachte: „Wie die alte Frau schnarcht. Da frage ich lieber mal nach, ob ihr was fehlt.“ Er trat in die Stube und sah, dass der Wolf im Bette lag. Nun wollte er sein Gewehr anlegen, jedoch fiel ihm ein, dass der Wolf ja die Großmutter gefressen haben könnte. Vielleicht konnte er sie noch retten. So packte er denn Wolf an Füßen und schüttelte ihn bis das Mädchen die Großmutter heraus purzelten. Der Wolf floh aus dem Haus und ließ sich nie wieder blicken.
    
     Da waren alle drei vergnügt. Die Großmutter aß den Kuchen, trank den Wein und erholte sich wieder.
    
     Ende.

---
+++ app/src/main/java/com/example/infoappv2/ChatMainThing.kt
@@ -0,0 +1,62 @@
+package com.example.infoappv2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TableLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.example.infoappv2.ui.chat.ChatFragment
+import kotlinx.android.synthetic.main.activity_chat.*
+
+class ChatMainThing : AppCompatActivity() {
+
+    fun OnCreate(
+        inflater: LayoutInflater,
+        savedInstanceState: Bundle?
+    ){
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_chat)
+        setSupportActionBar(toolbar_chat)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_chat)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = ""
+
+        val tabLayout : TableLayout = findViewById(R.id.tab_Layout)
+        val viewPager : ViewPager = findViewById(R.id.View_Pager)
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+
+        viewPagerAdapter.addFragment(ChatFragment(), "Chats")
+
+        viewPager.adapter = viewPagerAdapter
+    }
+
+    internal class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager)
+    {
+        private val fragments: ArrayList<Fragment> = ArrayList<Fragment>()
+        private val titles: ArrayList<String> = ArrayList<String>()
+
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        override fun getCount(): Int {
+            return fragments.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String){
+            fragments.add(fragment)
+            titles.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return titles[position]
+        }
+    }
+
+}
