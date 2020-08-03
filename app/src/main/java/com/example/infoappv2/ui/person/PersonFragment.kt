3a41898ee7df0c0b7fd8ed8f4638cf4c7523f06f
maxel <maximilian.elbers@hs-osnabrueck.de>
2020-08-03 15:55:22 +0200

    Personas Fragmentos Callientos

--- app/src/main/java/com/example/infoappv2/ui/person/PersonFragment.kt
+++ app/src/main/java/com/example/infoappv2/ui/person/PersonFragment.kt
@@ -1,5 +1,6 @@
 package com.example.infoappv2.ui.person
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -28,26 +29,58 @@ class PersonFragment : Fragment() {
             ViewModelProviders.of(this).get(PersonViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_person, container, false)
 
-        retrieveWebInfo()
         personViewModel.text.observe(viewLifecycleOwner, Observer {
 
         })
         return root
     }
 
+    @SuppressLint("ResourceType")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        retrieveWebInfo()
+    }
+
     private fun retrieveWebInfo() {
         thread {
-            val doc = Jsoup.connect("https://www.tutorialspoint.com/android/index.htm").get()
+            //Verbindung zur Hochschulseite
+            val url = "https://www.hs-osnabrueck.de/wir/fakultaeten/mkt/organisation/personen-a-z/?az=rolle:professor%2Ainnen/"
+            val doc = Jsoup.connect(url).get()
 
-            val imageElements = doc.getElementsByClass("img-responsive")
-            val textElements = doc.getElementsByTag("h1")
+            //Variable für die For-Schleife um die genaue Länge herauszulesen
+            val div1 = doc.getElementsByClass("az-item").size
+            println("Die länge beträgt:" + div1)
 
-            val  imageUrl = imageElements[0].absUrl("src")
+            val div = doc.getElementsByClass("az-list-inner-content").html()
+            val divOther = div.length
 
-            this.run{
-                textTitle1.text = textElements[0].text()
-                Picasso.get().load(imageUrl).into(imgTitle)
-            }
+
+//            val imgDiv = div.getElementsByClass("image-wrapper")
+//            val genDiv = div.getElementsByClass("general-Information")
+//            val conDiv = div.getElementsByClass("contact-information")
+//
+//            val img = imgDiv.
+//            val name = genDiv.select("[itemprop=name]")
+//            val fac = genDiv.select("p")
+//            val number = conDiv.select("[itemprop=telephone]")
+//            val mail = conDiv.select("[itemprop=email]")
+//            val room = conDiv.select("[itemprop=room]")
+//            val fax = conDiv.select("[itemprop=faxNumber]")
+
+//            val  imageUrl = img[0].absUrl("src")
+//
+//            this.run{
+//
+//                Picasso.get().load(imageUrl).into(persImage)
+//                persName.text = name.text()
+//                persFac.text = fac.text()
+//                pers_call.text = number.text()
+//                pers_email.text = mail.text()
+//                pers_room.text = room.text()
+//                pers_fax.text = fax.text()
+
+//            }
         }
     }
 }
